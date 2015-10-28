15230dac8cb8e2b9cea8cfb8625a7595dc4205c5
Devendra <devendra@udgama.com>
2015-10-29 00:05:02 +0530

    timeout test

---
+++ web/examples/timeout_test.js
@@ -0,0 +1,62 @@
+
+
+var publish_key = "demo";
+var subscribe_key = "demo";
+var origin = "ps1.pubnub.com";
+
+
+var pubnub_arr = [];
+var timeouts = 0;
+var errors = 0;
+var total_messages = 0;
+
+
+function publish(pubnub, channel, message, count, index, iteration) {
+    setTimeout(function(){
+        pubnub.publish({
+            'channel' : channel + '-' + count,
+            'message' : message + '-' + count,
+            'callback' : function(r){
+                ++total_messages;
+                ++count;
+                if (count <= 200) {
+                    publish(pubnub, channel, message, count, index, iteration);
+                } else {
+                    console.log("Finshed, error count " + errors + 
+                        ", Timeouts : " + timeouts + ", PN Count " + pubnub_arr.length
+                        + ", count = " + count + ", index " + index + ", iteration = " + iteration + ", Total = " + total_messages);
+                }
+
+            },   
+            'error' : function(r) {
+                if (r['message'] === "timeout") {
+                    timeouts++;
+                    if (timeouts == 1) {
+                        console.log("timeout happened at " + pubnub_arr.length);
+                    }
+                    errors++;
+                }
+            }         
+        })
+    }, 50);
+}
+
+function test(i) {
+    setTimeout(function(){
+        pubnub_arr[i] = PUBNUB.init({
+            'origin' : origin,
+            'publish_key' : publish_key,
+            'subscribe_key' : subscribe_key,
+        });
+
+        for (var j = 1 ; j <= pubnub_arr.length; j++) {
+            var pn = pubnub_arr[j];
+            pn && publish(pn, "channel", "message", 0, j, i);
+        }
+    }, 120000);
+}
+
+for (var i = 1; i <= 20; i++) {
+    test(i);
+}
+
